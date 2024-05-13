dc70fb2a24db5e17bb416c04e265cfbe241ae1a9
miaskor <misha.skorohodv@gmail.com>
2024-05-13 14:11:29 +0300

    feat(domain-api): add prometheus and grafana

--- domain/domain-api/src/main/kotlin/by/miaskor/domain/api/config/AppConfig.kt
+++ domain/domain-api/src/main/kotlin/by/miaskor/domain/api/config/AppConfig.kt
@@ -1,5 +1,8 @@
 package by.miaskor.domain.api.config
 
+import io.micrometer.core.instrument.MeterRegistry
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -10,5 +13,14 @@ open class AppConfig {
   @Bean
   open fun passwordEncoder(): BCryptPasswordEncoder {
     return BCryptPasswordEncoder()
+  }
+
+  @Bean
+  open fun configurer(
+    @Value("\${spring.application.name}") applicationName: String?,
+  ): MeterRegistryCustomizer<MeterRegistry> {
+    return MeterRegistryCustomizer { registry: MeterRegistry ->
+      registry.config().commonTags("application", applicationName)
+    }
   }
 }
